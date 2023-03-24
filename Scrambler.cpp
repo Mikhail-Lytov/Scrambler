315023052853e20bd318cfa596dc5fc08370e479
Lytov <misha.lytov@yande.xru>
2023-03-24 18:25:28 +0300

    Scrambler

---
+++ Scrambler.cpp
@@ -0,0 +1,43 @@
+
+
+#include <iostream>
+using namespace std;
+
+string scrambling(string line, int first_offset, int second_offset ) {
+    int *input_data = new int[line.length()];
+    int *output_data = new int[line.length()];
+    string line_output = "";
+
+    for (int i = 0; i < line.length(); i++) {
+        if ((int)line[i] == 49) {
+            input_data[i] = 1;
+        }
+        else {
+            input_data[i] = 0;
+        }
+    }
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (i < first_offset) {
+            output_data[i] = (int)input_data[i];
+        }
+        else if (i < second_offset) {
+            output_data[i] = input_data[i] ^ output_data[i - first_offset];
+        }
+        else {
+            output_data[i] = input_data[i] ^ output_data[i - first_offset] ^ output_data[i - second_offset];
+        }
+
+    }
+    for(int i = 0; i <line.length(); i++){
+        line_output += to_string(output_data[i]);
+    }
+    return line_output;
+}
+int main()
+{
+    string line_2 = "1111111110000000";
+    cout << scrambling(line_2, 3,5);
+
+}
+
---
+++ Scrambler_int.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int scrambling(unsigned data, int first_offset, int second_offset ) {
+    string line = "";
+    int size = 0;
+
+    do {
+        line.push_back('0' + (data & 1));
+    }while (data >>= 1);
+    reverse(line.begin(), line.end());
+    int input_data[15];
+    int output_data[15];
+    int output = 0;
+
+    for (int i = 0; i < line.length(); i++) {\
+        if ((int)line[i] == 49) {
+            input_data[i] = 1;
+        }
+        else {
+            input_data[i] = 0;
+        }
+    }
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (i < first_offset) {
+            output_data[i] = (int)input_data[i];
+        }
+        else if (i < second_offset) {
+            output_data[i] = input_data[i] ^ output_data[i - first_offset];
+        }
+        else {
+            output_data[i] = (input_data[i] ^ output_data[i - first_offset]) ^ output_data[i - second_offset];
+        }
+
+    }
+    for(int i = 0; i < line.length();i++){
+        cout << output_data[i];
+    }
+    cout << "\n";
+    for(int i = line.length() - 1; i > -1; i--){
+        output += pow(2,size++) * output_data[i];
+    }
+    return output;
+}
+
+int main(){
+
+    cout << scrambling(29273,3,5); // 32640
+    cout << "\n t" <<(1 ^ 0 ^ 0);
+    return 0;
+}
